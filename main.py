79f6e7e00dbf2eb87592785e9483f975eb2259bb
Carlos Gomez <salos_12@hotmail.com>
2018-04-01 19:53:57 -0400

    perceptron class and training data processing

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import sys
+import csv
 
 from perceptron import Perceptron
 
 inputFile = sys.argv[1]
 outputFile = sys.argv[2]
+features = []
 
-solver = Solver()
-if method == 'bfs':
-    solver.bfs(initialState)
-elif method == 'dfs':
-    solver.dfs(initialState)
-elif method == 'ast':
-    solver.ast(initialState)
+with open(inputFile, 'r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    for row in spamreader:
+        features += row
+
+perceptron = Perceptron()
+perceptron.train(features)
--- perceptron.py
+++ perceptron.py
@@ -1,5 +1,16 @@
 
 
 class Perceptron:
-    def train(self):
-        pass 
+    def __init__(self):
+        self.weigthVectors = [0,0,0]
+
+    def train(self,features):
+        if len(features) > 0:
+            for feature in features:
+        else:
+            print("No features provided.")
+
+    def stepFunction(self, features):
+        sumatory = 0
+        for i in range(len(self.weigthVectors)+1):
+            sumatory = self.weigthVectors[i]
