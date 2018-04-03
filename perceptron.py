e86860d3598c6f36e42c072eaa5fa70f74c45a20
Carlos Gomez <carlos@atl-software.net>
2018-04-03 13:00:33 -0400

    Update weights after error

--- perceptron.py
+++ perceptron.py
@@ -1,26 +1,28 @@
-
+import numpy as np
 
 class Perceptron:
     def __init__(self):
         self.weigthVectors = [0,0,0]
-        self.exampleVector = []
+        self.exampleVector = ()
+        self.trueLabels = []
 
     def train(self,examples):
         if len(examples) > 0:
             for example in examples:
                 features = example.split(',')
-                self.exampleVector.append((features[0],features[1], features[2]))
+                self.exampleVector.append((1,features[0],features[1]))
+                self.trueLabels.append(features[2])
             previousWeigthVector = [0,0,0]
             while(True):
                 for exampleIndex in range(len(self.exampleVector)+1):
-                    trueLabel = self.exampleVector[exampleIndex][2]
+                    trueLabel = self.trueLabels[exampleIndex]
                     sfResult = trueLabel * self.stepFunction(exampleIndex)
                     if sfResult <= 0:
+                        previousWeigthVector = list(self.weigthVectors)
                         for weightVector in self.weigthVectors:
-                            weightVector += trueLabel * 
-                        previousWeigthVector = 
-
-
+                            weightVector += trueLabel * np.array(self.exampleVector[exampleIndex])
+                if previousWeigthVector != self.weigthVectors:
+                    break
         else:
             print("No features provided.")
 
